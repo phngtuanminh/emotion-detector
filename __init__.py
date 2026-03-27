6589d31d8123da14e3b0478f1aecbc258c55039e
phngtuanminh <phngtuanminh0227@gmail.com>
2026-03-27 08:06:34 +0700

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""
+__init__.py
+Task 4 – Activity 1
+Package Initialization — imports the emotion_detector function
+Project: EmoSense – AI Emotion Detector
+Author: phngtuanminh
+"""
+
+from . emotion_detection import emotion_detector
